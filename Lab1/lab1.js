56a8a63ea803eb288583caf2c538880517f112c7
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2022-09-17 17:05:58 +0300

    Lab1 first half

---
+++ Lab1/lab1.js
@@ -0,0 +1,32 @@
+function Singleton(param){
+    let list=[param];
+    return list;
+}
+console.log(Singleton("Task one"));
+
+function Null(list){
+    if(list.length===0){
+        return false;
+    }else{
+        return true;
+    }
+}
+console.log(Null([]));
+
+function snoc(list, elem){
+
+    list.push(elem);
+    return list;
+}
+console.log(snoc(["old island", "simple island"], "new island"));
+
+function length(list){
+    let i=0;
+    list=[1, 2, 3]
+    for(let elem of list){
+        i++;
+    }
+    console.log(i);
+    return i;
+}
+length();
